545a782f8e0ae7236e654a5d8cec46368b4a4de9
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-08 03:15:11 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,7 +7,14 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.new
+
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
 
     redirect_to author_path(@author)
   end
